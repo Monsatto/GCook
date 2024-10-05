654f313ca9fc30b49e283c7b6c2c7335fa8e7c98
Monsatto <tiagoguero11@gamil.com>
2024-10-04 22:27:41 -0300

    Gcook - Segunda parte

--- GCook/Models/Comentario.cs
+++ GCook/Models/Comentario.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace GCook.Models
+namespace GCook.Models;
 
 [Table("Comentario")]
 public class Comentario
--- GCook/Models/Usuario.cs
+++ GCook/Models/Usuario.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
-namespace GCook.Models
+namespace GCook.Models;
 
 [Table("Usuario")]
     public class Usuario
